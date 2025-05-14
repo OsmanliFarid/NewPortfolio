d43d7605c12f054eb537bf6c945bab04b82b71cb
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-15 02:30:38 +0400

    react.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <div className="Container">
+        <Navbar />
+      </div>
+    </>
+  );
+};
+export default App;
