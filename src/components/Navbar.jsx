4bbfdfc43a7ff47bdf5acd85fec8c3ae3738282f
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-16 03:22:58 +0400

    Navbar component no change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,54 +1,76 @@
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+
 const Navbar = () => {
   const Links = [
     { id: 1, href: "./index.html", title: "Home" },
     { id: 2, href: "#Service", title: "Services" },
-    { id: 3, href: "#", title: "About me" },
+    { id: 3, href: "#About", title: "About me" },
     { id: 4, href: "#", title: "Portfolio" },
     { id: 5, href: "#", title: "Contact me" },
   ];
+
+  const [active, SetActive] = useState(1);
+  const [Bars, SetBars] = useState(false);
+
+  const Show = () => {
+    SetBars(!Bars);
+  };
+
   return (
     <>
-      <div className="grid grid-cols-3 h-[120px] items-center">
-        <div className="Logo">
-          <img src="/public/Logo.png" alt="" />
+      <div
+        className="grid grid-cols-3 h-[120px] items-center md:fixed md:z-10"
+        id="Index"
+      >
+        <div className="Bars md:hidden">
+          <FaBars className="text-[24px]" onClick={Show} />
         </div>
-        <div className="Links flex gap-[30px] justify-center">
-          {Links.map(({ href, id, title }, index) => {
-            return (
-              <a href="">
-                {title === "Home" ? (
-                  <a
-                    className="text-[#FD6F00] "
-                    key={index}
-                    href="./index.html"
-                  >
-                    {title}
-                  </a>
-                ) : (
-                  <a
-                    className="text-[#858585] font-[500] font-[Lato] "
-                    key={index}
-                    href={href}
-                  >
-                    {title}
-                  </a>
-                )}
-              </a>
-            );
-          })}
+        <div className="Logo" id="Logo">
+          <a href="./index.html">
+            <img src="/public/Logo.png" alt="" />
+          </a>
         </div>
-        <div className="grid justify-end">
-          <div className="">
+
+        <div className="Links hidden gap-[30px] justify-center md:flex">
+          {Links.map(({ href, id, title }) => (
             <a
-              href="#"
-              className="bg-[#FD6F00] p-[10px_35px] rounded-[10px] font-[Lato] text-[#fff]"
+              key={id}
+              className={id === active ? "text-[#FD6F00]" : "text-[#858585]"}
+              href={href}
+              onClick={() => SetActive(id)}
             >
-              Hire Me
+              {title}
             </a>
-          </div>
+          ))}
         </div>
+
+        <div className="grid justify-end">
+          <a
+            href="#"
+            className="bg-[#FD6F00] p-[5px_20px] rounded-[10px] font-[Lato] text-[#fff]"
+          >
+            Hire Me
+          </a>
+        </div>
+
+        {Bars && (
+          <div className="fixed z-3 flex flex-col w-full text-center p-[10px_0px] bg-[#fff] right-0 bottom-0 md:hidden">
+            {Links.map(({ href, id, title }) => (
+              <a
+                key={id}
+                className={id === active ? "text-[#FD6F00]" : "text-[#858585]"}
+                href={href}
+                onClick={() => SetActive(id)}
+              >
+                {title}
+              </a>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
 };
+
 export default Navbar;
