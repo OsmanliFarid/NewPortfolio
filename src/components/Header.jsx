6340662d4f9d4ada2d0277caf59d3fa7df9ffced
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-16 03:22:44 +0400

    Header component  no change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const letterAnimation = {
 };
 
 const Header = () => {
-  const text = "Osmanli Farid";
+  const text = "";
   const letters = splitText(text);
 
   const [visibleCount, setVisibleCount] = useState(0);
@@ -54,18 +54,25 @@ const Header = () => {
   }, [visibleCount, isDeleting, letters.length, startAnimation]);
 
   return (
-    <div className="flex justify-between mt-[-10px]" ref={ref}>
+    <div
+      className="grid grid-cols-1 md:grid-cols-2 justify-between pt-[100px]"
+      ref={ref}
+    >
       <motion.div
         initial={{ x: -200, opacity: 0 }}
         animate={isInView ? { x: 0, opacity: 1 } : {}}
         transition={{ duration: 0.8 }}
       >
-        <div className="h-[600.65px] bg-[url('/HeaderGrad.png')] bg-no-repeat bg-center bg-cover w-[516.74px]">
-          <div className="font-[Lato] pt-[100px]">
-            <h1 className="text-[#858585] text-[24px]">Hi I am</h1>
-
+        <div className="max-h-[600.65px] bg-[url('/HeaderGrad.png')] bg-no-repeat bg-center bg-cover w-full max-w-[516.74px]">
+          <div className="font-[Lato] pt-[10px] md:pt-[100px] ">
+            <h1 className="text-[#858585] text-[16px] md:text-[24px] w-full text-center md:text-start">
+              Hi I am
+            </h1>
+            <h1 className=" text-[#6F6D6C] text-[20px] md:text-[28px] flex gap-1 overflow-hidden justify-center md:justify-start">
+              Osmanli Farid
+            </h1>
             <h1
-              className="text-[#6F6D6C] text-[28px] flex gap-1 overflow-hidden"
+              className=" text-[#6F6D6C] text-[20px] md:text-[28px] flex gap-1 overflow-hidden md:justify-start"
               style={{ minWidth: "400px", whiteSpace: "nowrap" }}
             >
               <AnimatePresence>
@@ -85,38 +92,52 @@ const Header = () => {
               </AnimatePresence>
             </h1>
 
-            <h1 className="text-[#FD6F00] text-[60px] w-[600px]">
+            <h1 className="text-[#FD6F00] text-[40px] md:w-[600px] md:text-[60px] w-full text-center md:text-start">
               Frontend Developer
             </h1>
 
-            <div className="flex items-center gap-10 mt-[30px] mb-[60px]">
+            <div className="flex justify-center md:justify-start items-center gap-10 mt-[30px] mb-[60px]">
               <a
                 href="#"
-                className="w-[156px] h-[48px] flex justify-center items-center bg-[#FD6F00] rounded-[10px] text-white"
+                className="text-[14px] w-[93px] h-[33px] md:w-[156px] md:h-[48px] flex justify-center items-center bg-[#FD6F00] rounded-[10px] text-white"
               >
                 Hire Me
               </a>
               <a
                 href="/Cv.pdf"
                 target="_blank"
-                className="w-[209px] h-[48px] rounded-[10px] text-[#6F6D6C] flex justify-center items-center border border-[#6F6D6C]"
+                className="text-[14px] w-[130px] h-[33px] md:w-[209px] md:h-[48px] rounded-[10px] text-[#6F6D6C] flex justify-center items-center border border-[#6F6D6C]"
               >
                 Download CV
               </a>
             </div>
 
-            <div className="w-[534px] h-[125px] bg-[rgba(27,27,27,0.6)] rounded-[10px] flex gap-5 items-center justify-around">
-              <div className="ml-[15px] border-r-[3px] pr-6 border-[#9B9B9B] h-[80px]">
-                <h1 className="text-[#FD6F00] text-[24px]">1+</h1>
-                <p className="text-[#9B9B9B] text-[20px]">Experiences</p>
-              </div>
-              <div className="border-r-[3px] pr-6 border-[#9B9B9B] h-[80px]">
-                <h1 className="text-[#FD6F00] text-[24px]">50+</h1>
-                <p className="text-[#9B9B9B] text-[20px]">Project done</p>
-              </div>
-              <div className="pr-6">
-                <h1 className="text-[#FD6F00] text-[24px]">1+</h1>
-                <p className="text-[#9B9B9B] text-[20px]">Happy Clients</p>
+            <div className="">
+              <div className="w-[384px] h-[99px] md:w-[534px] md:h-[125px] bg-[rgba(27,27,27,0.6)] rounded-[10px] flex gap-5 items-center justify-around">
+                <div className="ml-[15px] border-r-[3px] pr-6 border-[#9B9B9B]">
+                  <h1 className="text-[#FD6F00] text-[20px] md:text-[24px]">
+                    1+
+                  </h1>
+                  <p className="text-[#9B9B9B] text-[14px] md:text-[20px]">
+                    Experiences
+                  </p>
+                </div>
+                <div className="border-r-[3px] pr-6 border-[#9B9B9B]">
+                  <h1 className="text-[#FD6F00] text-[20px] md:text-[24px]">
+                    50+
+                  </h1>
+                  <p className="text-[#9B9B9B] text-[14px] md:text-[20px]">
+                    Project done
+                  </p>
+                </div>
+                <div className="pr-6">
+                  <h1 className="text-[#FD6F00] text-[20px] md:text-[24px]">
+                    1+
+                  </h1>
+                  <p className="text-[#9B9B9B] text-[14px] md:text-[20px]">
+                    Happy Clients
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -128,9 +149,15 @@ const Header = () => {
         animate={isInView ? { x: 0, opacity: 1 } : {}}
         transition={{ duration: 0.9 }}
       >
-        <div className="h-[618px] bg-[url('/HeaderGrad2.png')] bg-no-repeat bg-center bg-cover w-[618px]">
-          <div className="flex justify-center items-center pt-[30px]">
-            <img src="/man.png" alt="Man" />
+        <div className="flex">
+          <div className="mt-[50px] md:mt-[0px] w-[382.99px] h-[382.99px] md:h-[618px] bg-[url('/HeaderGrad2.png')] bg-no-repeat bg-center bg-cover md:w-[618px]">
+            <div className="flex justify-center items-center pt-[30px]">
+              <img
+                src="/man.png"
+                alt="Man"
+                className="w-[320px] md:w-[520px]"
+              />
+            </div>
           </div>
         </div>
       </motion.div>
