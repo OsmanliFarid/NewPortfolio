7960571dad08252bac9f24c4c3f7ab4c3d1257b8
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-15 21:47:53 +0400

    no change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Services from "./components/Services";
+import About from "./components/About";
 const App = () => {
   return (
     <>
       <div className="Container">
         <Navbar />
         <Header />
+        <Services />
+        <About />
       </div>
     </>
   );
