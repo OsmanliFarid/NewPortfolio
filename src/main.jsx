ea470ce1fd034a02e88dc09f7aa9804bc29e98a4
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-16 20:03:25 +0400

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <video
+      autoPlay
+      muted
+      loop
+      className="fixed top-0 left-0 w-full h-full object-cover -z-10"
+    >
+      <source src="/earth.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
