10ebf50f18caf856964d4951d218dd5a46093a2f
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-19 23:21:21 +0400

    I added the contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,19 @@
+const Contact = () => {
+  return (
+    <>
+      <div className="Font-[Lato]">
+        <h1 className="text-[#fff] text-[40px] text-center mt-[30px]">
+          Contact me
+        </h1>
+        <div>
+          <form method="get">
+            <input type="text" />
+            <input type="number" />
+            <input type="text" />
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Contact;
